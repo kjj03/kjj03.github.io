7a502c3846f69b1ddc34a0f06780e02f650e2954
kjj03 <132124762+kjj03@users.noreply.github.com>
2026-06-04 19:26:05 +0900

    firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// firebase-config.js
+// TODO: 집에 가셔서 Firebase 콘솔에서 발급받은 본인의 Config 값으로 덮어쓰세요.
+export const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
